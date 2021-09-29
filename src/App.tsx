45842b6d4370075115b922a38f0062f7a9fd86ba
JewonYeon <yjw9852@gmail.com>
2021-09-29 18:23:27 +0900

    Feat : 전역 CSS 초기화 및 변수 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import "./App.css";
+import "./App.module.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">안녕하세요</div>;
 }
 
 export default App;
