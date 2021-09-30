09e57516ff9af0fc9a68b35883a6880238ebd3cf
JewonYeon <yjw9852@gmail.com>
2021-10-01 01:45:09 +0900

    Feat : Route 설정

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+const Routes = () => {
+  return (
+    <Switch>
+      {/* <Route exact path="/" component={} />
+      <Route path="/meme-generaor" component={} />
+      <Redirect path="*" to="/" /> */}
+    </Switch>
+  );
+};
+
+export default Routes;
